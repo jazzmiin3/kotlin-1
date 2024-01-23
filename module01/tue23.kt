fd10f7bce26ad7f8251a38a3ea9c82aaf649865f
itse1329SP24 <157508153+itse1329SP24@users.noreply.github.com>
2024-01-23 11:32:21 -0600

    created module 1 code

---
+++ module01/tue23.kt
@@ -0,0 +1,13 @@
+fun main() { 
+    println("The hero announces her presence to the World.")
+    
+    val heroName: String = "Madrigal"
+    println(heroName)
+    
+    var playerLevel: Int = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+}
