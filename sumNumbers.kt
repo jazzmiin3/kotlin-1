309c9607ec65b732b6f6f51b053d3edbaa5d9677
jazzmiin3 <157508048+jazzmiin3@users.noreply.github.com>
2024-04-02 09:21:33 -0500

    Create sumNumbers.kt

---
+++ sumNumbers.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    var i = 1
+    while (i <= 50) {
+        println(i)
+        i++
+    }
+
+    for (i in 1..50) {
+        println(i)
+    }
+
+    i = 0
+    while (i <= 50) {
+        println("Numbers")
+        i++
+    }
+
+    i=0
+    println()
+    
+}
