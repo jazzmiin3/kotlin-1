b62ec098eb485e2b732dfd045110d76420e68f01
jazzmiin3 <157508048+jazzmiin3@users.noreply.github.com>
2024-04-30 10:03:54 -0500

    Create barChart.kt

---
+++ week 7/barChart.kt/barChart.kt
@@ -0,0 +1,31 @@
+fun main() {
+    val store1 = 1000
+    val store2 = 1200
+    val store3 = 1800
+    val store4 = 800
+    val store5 = 1900
+  for (i in 1..store1) {
+      println(i)
+  } 
+    println()
+    for (i in 1..store2) {
+        println(i)
+    }
+    println()
+    for (i in 1..store3) {
+        println(i)
+    }
+    println()
+    for (i in 1..store4) {
+        println(i)
+    }
+    println()
+    for (i in 1..store5) {
+        println(i)
+    }
+    println("Store 1: **********")
+    println("Store 2: ************")
+    println("Store 3: ******************")
+    println("Store 4: ********")
+    println("Store 5: *******************")
+}
